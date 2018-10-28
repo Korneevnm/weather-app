dcd678ac85696b82d73f384dcf5b795cff841c34
Nikita <Korneevnm@yandex.ru>
2018-10-28 17:38:18 +0400

    weather

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Main from './containers/Main';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
